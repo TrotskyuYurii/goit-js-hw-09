ea8834422b36ddefc6cddd30efddab888ac7ed23
TrotskyuYurii <tro2001@gmail.com>
2024-01-08 16:57:14 +0200

    task2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,35 +4,34 @@ let feedbackFormState = {
     message: ''
 }
 
-//Перевірка стану локального сховища при завантаженні сторінки
+// Перевірка стану локального сховища при завантаженні сторінки
 document.addEventListener('DOMContentLoaded', function () {
+    feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+    
+    if (feedbackFormState) {
+        document.querySelector('input[name="email"]').value = feedbackFormState.email || '';
+        document.querySelector('textarea[name="message"]').value = feedbackFormState.message || '';
+    }
+});
 
-    feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-     if (feedbackFormState) {
-      document.querySelector('input[name="email"]').value = feedbackFormState.email || '';
-      document.querySelector('textarea[name="message"]').value = feedbackFormState.message || '';
-     }
-  });
-
-
-//Записуємо у локальне сховище при кожній подіі ІНПУТ
-registerForm.addEventListener('input', function(event) {
+// Записуємо у локальне сховище при кожній події INPUT
+registerForm.addEventListener('input', function (event) {
     const target = event.target;
 
-    if (target.tagName === 'INPUT') {
-        feedbackFormState.email  = target.value;  
-    } else if (target.tagName === 'TEXTAREA') {
-        feedbackFormState.message  = target.value;
+    console.log(feedbackFormState);
+
+    if (target.tagName === 'INPUT' && target.name === 'email') {
+        feedbackFormState.email = target.value.trim();
+    } else if (target.tagName === 'TEXTAREA' && target.name === 'message') {
+        feedbackFormState.message = target.value.trim();
     }
 
     localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
-  });
+});
 
-
-//Відключаємо скидання форми дія за-замовченням
+// Відключаємо скидання форми дія за-замовченням
 registerForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  document.querySelector('form').reset();
-  console.log(feedbackFormState);
+    event.preventDefault();
+    document.querySelector('form').reset();
+    console.log(feedbackFormState);
 });
