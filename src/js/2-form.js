91f41bf09812b0557bd948a79ed0d1c5563b90c0
TrotskyuYurii <tro2001@gmail.com>
2024-01-08 17:11:46 +0200

    task2_JS_complete

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,21 +4,21 @@ let feedbackFormState = {
     message: ''
 }
 
+
 // Перевірка стану локального сховища при завантаженні сторінки
 document.addEventListener('DOMContentLoaded', function () {
     feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-    
+
     if (feedbackFormState) {
         document.querySelector('input[name="email"]').value = feedbackFormState.email || '';
         document.querySelector('textarea[name="message"]').value = feedbackFormState.message || '';
     }
 });
 
+
 // Записуємо у локальне сховище при кожній події INPUT
 registerForm.addEventListener('input', function (event) {
     const target = event.target;
-
-    console.log(feedbackFormState);
 
     if (target.tagName === 'INPUT' && target.name === 'email') {
         feedbackFormState.email = target.value.trim();
@@ -29,9 +29,19 @@ registerForm.addEventListener('input', function (event) {
     localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
 });
 
+
 // Відключаємо скидання форми дія за-замовченням
 registerForm.addEventListener('submit', (event) => {
+
     event.preventDefault();
-    document.querySelector('form').reset();
-    console.log(feedbackFormState);
+
+    if (document.querySelector('input[name="email"]').value.trim() !== '' && document.querySelector('textarea[name="message"]').value.trim() !== '') {
+        console.log(feedbackFormState);
+
+        document.querySelector('form').reset();
+        feedbackFormState.email='';
+        feedbackFormState.message='';
+        localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+    }
+
 });
